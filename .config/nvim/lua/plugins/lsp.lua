1dd72b10d683ae3a6ade31ea62cdd362860810a7
Yuval Dolev <mail@ydolev.com>
2024-05-30 23:41:21 +0300

    feat(nvim): update LSP configuration

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,70 +1,23 @@
 return {
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    lazy = true,
-    config = false,
-    init = function()
-      -- Disable automatic setup, we are doing it manually
-      vim.g.lsp_zero_extend_cmp = 0
-      vim.g.lsp_zero_extend_lspconfig = 0
-    end,
-  },
-  {
-    'williamboman/mason.nvim',
-    lazy = false,
-    config = true,
-  },
-
-  -- Autocompletion
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    event = {"BufReadPre", "BufNewFile"},
+    cmd = "Mason",
     dependencies = {
-      {'L3MON4D3/LuaSnip'},
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/nvim-cmp",
+      "L3MON4D3/LuaSnip",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "j-hui/fidget.nvim",
     },
     config = function()
-      -- Here is where you configure the autocompletion settings.
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_cmp()
-
-      -- And you can configure cmp even more, if you want to.
-      local cmp = require('cmp')
-      local cmp_action = lsp_zero.cmp_action()
-
-      cmp.setup({
-        completion = {
-          autocomplete = false,
-        },
-        formatting = lsp_zero.cmp_format(),
-        mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-        })
-      })
-    end
-  },
-
-  -- LSP
-  {
-    'neovim/nvim-lspconfig',
-    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-    event = {'BufReadPre', 'BufNewFile'},
-    dependencies = {
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'williamboman/mason-lspconfig.nvim'},
-      { "j-hui/fidget.nvim", opts = {} },
-    },
-    config = function()
-      -- This is where all the LSP shenanigans will live
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_lspconfig()
-
-      lsp_zero.on_attach(function(client, bufnr)
+      local lsp_zero = require("lsp-zero")
+      lsp_zero.on_attach(function(_, bufnr)
         local opts = {buffer = bufnr, remap = false}
+
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>ls", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -85,19 +38,33 @@ return {
         vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>ln", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+        vim.diagnostic.config({
+          virtual_text = true,
+        })
       end)
 
-      require('mason-lspconfig').setup({
-        ensure_installed = {
-          "lua_ls",
-          "rust_analyzer",
+      local cmp = require("cmp")
+      cmp.setup({
+        completion = {
+          autocomplete = false,
         },
+      })
+
+      local fidget = require("fidget")
+      fidget.setup({})
+
+      local mason = require("mason")
+      mason.setup({})
+
+      local mason_lspconfig = require("mason-lspconfig")
+      mason_lspconfig.setup({
+        ensure_installed = {"lua_ls", "tsserver", "rust_analyzer"},
         handlers = {
-          lsp_zero.default_setup,
-          lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require("lspconfig").lua_ls.setup(lua_opts)
+          function(server_name)
+            require("lspconfig")[server_name].setup({})
           end,
+
           rust_analyzer = function()
             require("lspconfig").rust_analyzer.setup({
               settings = {
@@ -113,24 +80,15 @@ return {
                   procMacro = {
                     enable = true
                   },
-                  inlayHints = {
-                    enable = true,
-                    showParameterNames = true,
-                  },
                   diagnostics = {
                     enable = true,
                   }
                 },
               },
             })
-          end
-        }
+          end,
+        },
       })
     end,
-    init = function()
-      vim.diagnostic.config({
-        virtual_text = true,
-      })
-    end
-  }
+  },
 }
